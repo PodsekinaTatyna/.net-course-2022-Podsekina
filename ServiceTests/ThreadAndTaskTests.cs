8551dc5f4784d87ef5ea37e96ed54e0d6ed76011
Podsekina Tatyna <tany.podsekina.02@bk.ru>
2022-10-12 17:36:41 +0300

    changePracticeByThreads fix: removed lock and added delay

--- ServiceTests/ThreadAndTaskTests.cs
+++ ServiceTests/ThreadAndTaskTests.cs
@@ -8,6 +8,7 @@ using Microsoft.VisualStudio.TestPlatform.Utilities;
 using Models;
 using ModelsDb;
 using Services;
+using Services.Filters;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -69,63 +70,48 @@ namespace ServiceTests
 
         [Fact]
         public void ReadingAndWritingCsv_InTwoDifferentStreams_Test()
-        {
-            
+        {                    
             Thread threadWritingCsv = new Thread(() =>
             {
+                ClientService clientService = new ClientService();
                 BankContext bankContext = new BankContext();
 
                 string directoryPath = Path.Combine("C:", "Users", "Professional", "Desktop", ".net-course-2022-Podsekina");
                 string fileName = "DataFromDbMultithreading.csv";
 
-                ExportService exportService = new ExportService(directoryPath, fileName);
+                ExportService exportServiceWritingCsv = new ExportService(directoryPath, fileName);
 
                 List<Client> clients = new List<Client>();
-                lock (locker)
-                {
-                    foreach (var client in bankContext.Clients)
-                    {
-                        clients.Add(new Client
-                        {
-                            Id = client.Id,
-                            FirstName = client.FirstName,
-                            LastName = client.LastName,
-                            PassportID = client.PassportID,
-                            DateOfBirth = client.DateOfBirth,
-                            Bonus = client.Bonus,
-                            PhoneNumber = client.PhoneNumber
-                        });
-                        Thread.Sleep(100);
-                    }
 
-                    exportService.WriteClientListToCsv(clients);
+                for (int i = 0; i < bankContext.Clients.Count(); i++)
+                {
+                    clients.Add(clientService.GetClients(new ClientFilter(), i + 1, 1).SingleOrDefault());
+
+                    _output.WriteLine($"{clients[i].Id} {clients[i].FirstName} {clients[i].LastName}");
+                    Thread.Sleep(100);
                 }
 
-                foreach (var client in clients)
-                {
-                    _output.WriteLine($"{client.Id} {client.FirstName} {client.LastName}");
-                }
-                
+                exportServiceWritingCsv.WriteClientListToCsv(clients);
+                                
             });
 
             Thread threadReadingCsv = new Thread(() =>
             {
                 ClientService clientService = new ClientService();
-
+                
                 string directoryPath = Path.Combine("C:", "Users", "Professional", "Desktop", ".net-course-2022-Podsekina");
                 string fileName = "DataToDbMultithreading.csv";
 
-                ExportService exportService = new ExportService(directoryPath, fileName);
+                ExportService exportServiceReadingCsv = new ExportService(directoryPath, fileName);
 
-                lock (locker)
+                List<Client> clientsFromCsv = exportServiceReadingCsv.ReadClientListFromCsv();
+
+                foreach (var client in clientsFromCsv)
                 {
-                    List<Client> clientsFromCsv = exportService.ReadClientListFromCsv();
-
-                    foreach (var client in clientsFromCsv)
-                    {
-                        clientService.AddNewClient(client);
-                    }
+                    clientService.AddNewClient(client);
+                    Thread.Sleep(100);
                 }
+                
             });
 
             threadWritingCsv.Start();
